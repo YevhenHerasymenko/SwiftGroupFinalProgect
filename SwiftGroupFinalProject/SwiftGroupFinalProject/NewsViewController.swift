f1a5efebe5ad279c0f0915b25a0aabda309646f9
Artem Orynko <a.orynko@gmail.com>
2016-08-25 07:48:01 +0300

    feat: add TabBar

---
+++ SwiftGroupFinalProject/SwiftGroupFinalProject/NewsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsViewController.swift
+//  SwiftGroupFinalProject
+//
+//  Created by Artem Orynko on 25.08.16.
+//  Copyright © 2016 Yevhen Herasymenko. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ SwiftGroupFinalProject/SwiftGroupFinalProject/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  SwiftGroupFinalProject
+//
+//  Created by Artem Orynko on 25.08.16.
+//  Copyright © 2016 Yevhen Herasymenko. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
